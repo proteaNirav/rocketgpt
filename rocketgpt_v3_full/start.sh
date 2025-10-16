bb2349270c1e629b9042bc891cdcd1634087c5dd
proteaNirav <29773482+proteaNirav@users.noreply.github.com>
2025-10-16 14:25:02 +0530

    Update start.sh

--- rocketgpt_v3_full/start.sh
+++ rocketgpt_v3_full/start.sh
@@ -8,5 +8,4 @@ pip install --no-cache-dir -r rocketgpt_v3_full/api/python/requirements.txt
 # run server (Railway sets $PORT; if not, default to 8080)
 PORT="${PORT:-8080}"
 cd rocketgpt_v3_full/api/python
-uvicorn main:app --host 0.0.0.0 --port 8080
-
+uvicorn main:app --host 0.0.0.0 --port "$PORT"
